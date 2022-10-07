ae61eb123e2c8750f58c19513866b0157c9d1817
João Moreira <joaopedromxxi@gmail.com>
2022-10-07 18:49:44 -0300

    Small alter in code

--- scripts/board.js
+++ scripts/board.js
@@ -3,51 +3,59 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
-context.fillStyle = "#161616";
+context.fillStyle = " #161616";
 context.fillRect(0, 0, 500, 305);
 
 //Gallows
 function showGallows() {
-  context.strokeStyle = "#333333";
+  context.strokeStyle = "#ff84b1dc";
   context.beginPath();
-  context.moveTo(110, 10);
-  context.lineTo(110, 305);
-  context.lineWidth = 24;
+  context.moveTo(15, 0);
+  context.lineTo(15, 300);
+  context.lineWidth = 4;
   context.stroke();
   context.beginPath();
-  context.moveTo(100, 24)
-  context.lineTo(245, 24);
-  context.lineWidth = 12;
+  context.moveTo(0, 20)
+  context.lineTo(500, 20);
+  context.lineWidth = 4;
   context.stroke();
   context.beginPath()
-  context.strokeStyle = "#ffffff";
-  context.moveTo(235, 30);
-  context.lineTo(235, 50);
-  context.lineWidth = 2;
+  context.moveTo(335, 0);
+  context.lineTo(335, 300);
+  context.lineWidth = 4;
   context.stroke();
-  context.beginPath()
-  context.strokeStyle = "#333333";
-  context.moveTo(80, 150);
-  context.lineTo(175, 150);
-  context.lineWidth = 16;
+  context.beginPath();
+  context.moveTo(175, 20);
+  context.lineTo(175, 55);
+  context.lineWidth = 4;
   context.stroke();
 }
 
 //Puppet
 //Head
 function showHead() {
-  context.fillStyle = '#9C9C9C';
   context.beginPath();
-  context.arc(235, 61, 14, 0, Math.PI * 2, true);
-  context.fill()
+  context.arc(175, 80, 26, 0, Math.PI * 2, true);
+  context.lineWidth = 2;
+  context.strokeStyle = "#ff84b1dc";
+  context.stroke();
+  context.beginPath();
+  context.arc(166, 74, 3, 0, Math.PI * 2, true);
+  context.fillStyle = "#ff84b1dc";
+  context.fill();
+  context.beginPath();
+  context.arc(184, 74, 3, 0, Math.PI * 2, true);
+  context.fill();
+  context.beginPath();
+  context.moveTo(184, 90);
+  context.lineTo(166, 90);
+  context.stroke();
 }
 //Body
 function showBody() {
-  context.strokeStyle = '#9C9C9C';
   context.beginPath();
-  context.moveTo(235, 70);
-  context.lineTo(235, 120);
-  context.lineWidth = 4;
+  context.moveTo(175, 106);
+  context.lineTo(175, 220);
   context.stroke();
 }
 
@@ -55,39 +63,30 @@ function showBody() {
 //Right
 function showRightArm() {
   context.beginPath();
-  context.strokeStyle = '#9C9C9C';
-  context.moveTo(235, 78);
-  context.lineTo(220, 96);
-  context.lineWidth = 4;
+  context.moveTo(175, 125);
+  context.lineTo(200, 158);
   context.stroke();
 }
 //Left
 function showLeftArm() {
   context.beginPath();
-  context.strokeStyle = '#9C9C9C';
-  context.moveTo(235, 78);
-  context.lineTo(250, 95);
-  context.lineWidth = 4;
+  context.moveTo(175, 125);
+  context.lineTo(150, 158);
   context.stroke();
-
 }
 
 //Legs
 //Right
 function showRightLeg() {
   context.beginPath();
-  context.strokeStyle = '#9C9C9C';
-  context.moveTo(235, 119);
-  context.lineTo(220, 129);
-  context.lineWidth = 4;
+  context.moveTo(175, 220);
+  context.lineTo(200, 250);
   context.stroke();
 }
 //Left
 function showLeftLeg() {
   context.beginPath();
-  context.strokeStyle = '#9C9C9C';
-  context.moveTo(235, 119);
-  context.lineTo(250, 129);
-  context.lineWidth = 4;
+  context.moveTo(175, 220);
+  context.lineTo(150, 250);
   context.stroke();
 }
